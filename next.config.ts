232a5b804615b30ac26f861a9c54c22b4d8a0fed
Elias Seddiki <eliasseddiki@MacBook-Pro-de-Elias.local>
2024-12-10 18:55:31 +0100

    Résoudre les conflits de fusion

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
+// next.config.js
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Ajoutez un slash final à chaque route pour la compatibilité avec GitHub Pages
+  trailingSlash: true,
+
+  // Activez l'exportation statique
+  output: 'export',
+
+  // Autres configurations Next.js si nécessaire
 };
 
 export default nextConfig;
