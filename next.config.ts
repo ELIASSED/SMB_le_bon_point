6cd68a7ed1fb232e3b00afa7a747a73f77901d70
Elias Seddiki <eliasseddiki@MacBook-Pro-de-Elias.local>
2025-04-09 15:26:12 +0200

    Supprime fichier volumineux et met à jour .gitignore

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack: (config, { isServer }) => {
-      // Ignorer les fichiers .map
-      config.module.rules.push({
-        test: /\.js\.map$/,
-        use: 'ignore-loader',
-      });
-  
-      // Exclure chrome-aws-lambda du bundle côté serveur
-      if (isServer) {
-        config.externals.push('chrome-aws-lambda');
-      }
-  
-      return config;
-    },
-    // Autres options Next.js si nécessaire
-    reactStrictMode: true,
-  };
-  
-  module.exports = nextConfig;
+// next.config.ts
+import type { NextConfig } from 'next';
+import type { WebpackConfigContext } from 'next/dist/server/config-shared';
+
+const nextConfig: NextConfig = {
+    webpack: (config: Configuration, { isServer }: WebpackConfigContext) => {    // Ignorer les fichiers .map
+    config.module.rules.push({
+      test: /\.js\.map$/,
+      use: 'ignore-loader',
+    });
+
+    // Exclure chrome-aws-lambda ou @sparticuz/chromium du bundle côté serveur
+    if (isServer) {
+      config.externals.push('@sparticuz/chromium');
+    }
+
+    return config;
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['@sparticuz/chromium', 'puppeteer-core'],
+  },
+};
+
+export default nextConfig;
